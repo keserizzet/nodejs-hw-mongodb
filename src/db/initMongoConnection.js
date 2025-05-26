79aa69c1e9104402c2c1843c6ea1363325b98ba0
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-05-26 10:41:54 +0300

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,24 +1,48 @@
+// src/db/initMongoConnection.js
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function initMongoConnection() {
   const {
     MONGODB_USER,
     MONGODB_PASSWORD,
-    MONGODB_URL,
+    MONGODB_HOST,
     MONGODB_DB,
   } = process.env;
 
-  if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_URL || !MONGODB_DB) {
-    throw new Error('MongoDB bağlantı bilgileri eksik!');
+  // Debug: env değerlerini kontrol edin
+  console.log('ENV MONGODB_USER:', MONGODB_USER);
+  console.log('ENV MONGODB_PASSWORD:', MONGODB_PASSWORD ? '***' : MONGODB_PASSWORD);
+  console.log('ENV MONGODB_HOST:', MONGODB_HOST);
+  console.log('ENV MONGODB_DB:', MONGODB_DB);
+
+  if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_HOST || !MONGODB_DB) {
+    console.error('❌ Eksik veya hatalı .env ayarı. Lütfen .env dosyanızı kontrol edin.');
+    process.exit(1);
   }
 
-  const uri = `mongodb+srv://${MONGODB_USER}:${encodeURIComponent(
-    MONGODB_PASSWORD
-  )}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+  const uri = [
+    'mongodb+srv://',
+    MONGODB_USER,
+    ':',
+    encodeURIComponent(MONGODB_PASSWORD),
+    '@',
+    MONGODB_HOST,
+    '/',
+    MONGODB_DB,
+    '?retryWrites=true&w=majority',
+  ].join('');
 
-  await mongoose.connect(uri);
-
-  console.log('Mongo connection successfully established!');
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ Mongo connection successfully established!');
+  } catch (err) {
+    console.error('❌ MongoDB bağlantı hatası:', err);
+    process.exit(1);
+  }
 }
 
 module.exports = initMongoConnection;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 // src/index.js
 require('dotenv').config();
-
 const initMongoConnection = require('./db/initMongoConnection');
 const setupServer = require('./server');
 
-async function main() {
-  try {
-    await initMongoConnection();
-    setupServer();
-  } catch (err) {
-    console.error('Uygulama başlatılamadı:', err);
-    process.exit(1);
-  }
-}
-
-main();
+(async () => {
+  await initMongoConnection();
+  setupServer();
+})();
