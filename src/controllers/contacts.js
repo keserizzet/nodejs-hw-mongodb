d559dd86411b9595614543728cc5b9fde6e3f746
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-05-27 11:54:17 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,15 +1,17 @@
+// src/controllers/contacts.js
 const { getAllContacts, getContactById } = require('../services/contacts');
 
 async function handleGetAllContacts(req, res, next) {
   try {
     const contacts = await getAllContacts();
-    res.status(200).json({
+    return res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
   } catch (err) {
-    next(err);
+    console.error('Error in handleGetAllContacts:', err);
+    return next(err);
   }
 }
 
@@ -20,13 +22,14 @@ async function handleGetContactById(req, res, next) {
     if (!contact) {
       return res.status(404).json({ message: 'Contact not found' });
     }
-    res.status(200).json({
+    return res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   } catch (err) {
-    next(err);
+    console.error('Error in handleGetContactById:', err);
+    return next(err);
   }
 }
 
